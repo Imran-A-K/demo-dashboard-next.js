5d9875d04411e933b68186e667c26e6429b10e89
unknown <imran.k.professional@gmail.com>
2023-08-09 13:17:57 +0600

    removed unnecessary codes

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,26 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {/* <main className="flex max-[850px]:flex-col h-screen">
-          <aside className="flex-none w-full md:w-52 relative">
-            <LeftSideBar
-              sideNavIsOpen={sideNavIsOpen}
-              setSideNavIsOpen={setSideNavIsOpen}
-            />
-          </aside>
-          <section className="shrink w-full max-sm:h-screen bg-sky-100/20 p-10">
-            <div className="sm:px-10">
-              <Title
-                sideNavIsOpen={sideNavIsOpen}
-                setSideNavIsOpen={setSideNavIsOpen}
-              />
-            </div>
-            {children}
-          </section>
-        </main> */}
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
