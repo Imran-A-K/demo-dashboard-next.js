887218dab9499cbf8c942e016d7095ce570effae
unknown <imran.k.professional@gmail.com>
2023-08-09 10:51:55 +0600

    added loader

--- app/page.jsx
+++ app/page.jsx
@@ -5,8 +5,9 @@ function Page() {
   useEffect(() => {
     redirect("/dashboard/doctors");
   }, []);
+
   return (
-    <div className="h-screen z-50 flex justify-center items-start pt-6">
+    <div className="h-screen z-50 flex justify-center items-start pt-24">
       <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900"></div>
     </div>
   );
